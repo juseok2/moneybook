91a08d926ed6465dd1bd400fdcb795b51d2f2e60
kjs <kjs@wace.me>
2026-07-27 10:44:24 +0900

    feat:지출 조회 목록

---
+++ app/expenses/page.tsx
@@ -0,0 +1,24 @@
+import pool from "@/lib/db";
+
+export default async function Page() {
+  const expensesList = await pool.query(
+    "select * from expenses order by spent_at desc",
+  );
+
+  return (
+    <ul>
+      {expensesList.rows.map((item) => {
+        return (
+          <li key={item.id}>
+            <p>지출명: {item.memo}</p>
+            <p>금액: {item.amount}</p>
+            <p>
+              지출일: {item.spent_at.getFullYear()}년{" "}
+              {item.spent_at.getMonth() + 1}월{item.spent_at.getDate()}일
+            </p>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
