faa0bec9a2a19911270a2fdb103b11785a97a1cd
kjs <kjs@wace.me>
2026-07-28 18:13:37 +0900

    feat: 월 정보 받기 및 검증 로직,기본값 설정

--- app/expenses/page.tsx
+++ app/expenses/page.tsx
@@ -3,30 +3,51 @@ import { deleteExpense } from "@/app/expenses/actions";
 import Link from "next/link";
 export const dynamic = "force-dynamic";
 
-export default async function Page() {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ month?: string }>;
+}) {
   const expensesList = await pool.query(
     "select * from expenses order by spent_at desc",
   );
 
+  const today = new Date();
+  const params = await searchParams;
+  let month = params.month;
+
+  if (month === undefined || !/^\d{4}-(0[1-9]|1[0-2])$/.test(month)) {
+    month =
+      String(today.getFullYear()) +
+      "-" +
+      (today.getMonth() + 1 < 10
+        ? "0" + String(today.getMonth() + 1)
+        : String(today.getMonth() + 1));
+  }
+
   return (
-    <ul>
-      {expensesList.rows.map((item) => {
-        return (
-          <li key={item.id}>
-            <p>지출명: {item.memo}</p>
-            <p>금액: {item.amount}</p>
-            <p>
-              지출일: {item.spent_at.getFullYear()}년{" "}
-              {item.spent_at.getMonth() + 1}월{item.spent_at.getDate()}일
-            </p>
-            <form action={deleteExpense}>
-              <input type="hidden" value={item.id} name="id" />
-              <button type="submit">삭제</button>
-            </form>
-            <Link href={`/expenses/${item.id}/edit`}>수정</Link>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <p>{month} 지출</p>
+      <br />
+      <ul>
+        {expensesList.rows.map((item) => {
+          return (
+            <li key={item.id}>
+              <p>지출명: {item.memo}</p>
+              <p>금액: {item.amount}</p>
+              <p>
+                지출일: {item.spent_at.getFullYear()}년{" "}
+                {item.spent_at.getMonth() + 1}월{item.spent_at.getDate()}일
+              </p>
+              <form action={deleteExpense}>
+                <input type="hidden" value={item.id} name="id" />
+                <button type="submit">삭제</button>
+              </form>
+              <Link href={`/expenses/${item.id}/edit`}>수정</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 }
